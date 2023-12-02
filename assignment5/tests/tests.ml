447978ef925774fe652de1dbd916169dea3baec6
Arijit Nukala <88689186+tikicode@users.noreply.github.com>
2023-12-02 04:05:58 -0500

    Add EC

--- assignment5/tests/tests.ml
+++ assignment5/tests/tests.ml
@@ -1,4 +1,21 @@
-(* 
-open Core;;
-open OUnit2;;
-*)
+open Core
+open OUnit2
+open Monads
+
+let test_are_balanced_monadic _ = 
+  assert_equal true @@ are_balanced_monadic "()";
+  assert_equal false @@ are_balanced_monadic "("
+
+let test_are_balanced_more_monadic _ =
+  assert_equal true @@ are_balanced_more_monadic "()";
+  assert_equal false @@ are_balanced_more_monadic "("
+
+let monad_tests =
+  "Monad tests"
+  >::: [
+         "are_balanced_monadic" >:: test_are_balanced_monadic;
+         "are_balanced_more_monadic" >:: test_are_balanced_more_monadic;
+       ]
+
+let series = "Assignment5 Tests" >::: [ monad_tests ]
+let () = run_test_tt_main series
